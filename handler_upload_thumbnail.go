0874290996bb87aa9ed1a613d96c50fec2d57d93
zidariu-sabin <zidariu19@gmail.com>
2025-09-24 23:31:09 +0300

    wip upload thumbnail handler

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +29,39 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+
+	//max number of bytes to story in memory per part in MultiPartForm of 10 MB
+	const maxMemory = 10 << 20 //equal to 10 * 2^20
+	r.ParseMultipartForm(maxMemory)
+
+	file, header, err := r.FormFile("thumbnail")
+
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+	}
+
+	mediaType := header.Header.Get("Content-Type")
+
+	imageData, err := io.ReadAll(file)
+
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to to read file data", err)
+	}
+
+	videoMetadata, err := cfg.db.GetVideo(videoID)
+
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to fetch video metadata", err)
+	}
+
+	if userID != videoMetadata.UserID {
+		respondWithError(w, http.StatusUnauthorized, "Unauthorized", err)
+	}
+
+	var thumbnail thumbnail = // see how to parse map here
 
 	respondWithJSON(w, http.StatusOK, struct{}{})
 }
